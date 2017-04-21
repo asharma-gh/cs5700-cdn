57df0227dbd059ee199d8777d53c783bf372f0f4
Christopher Brown <cebrown5597@gmail.com>
2017-04-21 18:06:41 -0400

    this script pre-caches wikipedia

---
+++ pre-cache.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir www
+while read LINE; do
+    curl --raw -D - http://ec2-54-166-234-74.compute-1.amazonaws.com:8080$LINE > www/"$(echo -n "$LINE" | sha256sum | head -c 64)"
+    if [ "$(du www/ | grep -o [0-9]*)" -gt 7680 ]; then
+        rm www/$(echo -n "$LINE" | sha256sum | head -c 64)
+        break
+    fi
+done < pre-cache.txt
+
+touch .cache_done

8b2ad3b072552fbc1d141a1d73707473e2e085fc
Christopher Brown <cebrown5597@gmail.com>
2017-04-21 18:51:12 -0400

    fixed some hashing problems

--- pre-cache.sh
+++ pre-cache.sh
@@ -2,11 +2,12 @@
 
 mkdir www
 while read LINE; do
-    curl --raw -D - http://ec2-54-166-234-74.compute-1.amazonaws.com:8080$LINE > www/"$(echo -n "$LINE" | sha256sum | head -c 64)"
+    curl --raw -sD - http://ec2-54-166-234-74.compute-1.amazonaws.com:8080/$LINE > www/"$(echo -n "/$LINE" | sha256sum | head -c 64)"
     if [ "$(du www/ | grep -o [0-9]*)" -gt 7680 ]; then
-        rm www/$(echo -n "$LINE" | sha256sum | head -c 64)
+        rm www/$(echo -n "/$LINE" | sha256sum | head -c 64)
         break
     fi
 done < pre-cache.txt
 
 touch .cache_done
+echo "Done downloading pre-cache"
